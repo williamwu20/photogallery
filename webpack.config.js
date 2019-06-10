afb8f6dd37c44d180937b0ac785ee92f7376fd02
williamwu20 <invisisharky@gmail.com>
2019-06-09 17:00:03 -0700

    add rules for scss, js, jsx, and img files

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,63 @@
 var path = require('path');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'main.bundle.js'
+        filename: 'bundle.[hash].js'
     },
     module: {
         rules: [
             {
-                test: /\.js(x*)$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
+                test: /\.(js(x*))$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [ '@babel/preset-env' ]
+                  }
                 }
-            },
+              },
             {
-                test: /\.(s*)css$/,
-                use: [{
-                    loader: 'css-loader'
-                }]
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+                
             },
             {
                 test: /\.(png|jp(e*)g|gif)$/,
-                use: [
-                    'url-loader',
-                    'file-loader'
-                ]
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: './[name].[ext]'
+                    } 
+                }]
+            },
+            {
+                test: /\.svg$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 10000,
+                        name: './[name].[ext]'
+                    } 
+                }]
             }
         ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+          }),
+    ],
     stats: {
         colors: true
     },
