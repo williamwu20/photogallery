76e723a6126fb9418a6af796d5805c3e6fceba8d
williamwu20 <invisisharky@gmail.com>
2019-06-11 12:06:15 -0700

    remove unused state in component

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -3,28 +3,19 @@ import ReactSwipe from 'react-swipe'
 import PropTypes from 'prop-types'
 
 export default class PhotoGallery extends React.Component {
-    state = {
-        photos: []
-    }
-    reactSwipeEl;
-    componentDidMount() {
-        this.setState({photos: this.props.photos})
-    }
     render() {
         return (
             <div>
                 <ReactSwipe
                     className='carousel'
                     swipeOptions={{ continuous: true }}
-                    childCount={this.state.photos.length}
+                    childCount={this.props.photos.length}
                     ref={el => (this.reactSwipeEl = el)}
                 >
-                    {this.state.photos.map(photo => (
+                    {this.props.photos.map(photo => (
                     <div className='item'> 
                         <img src={photo.url} />
-                        <div className='caption'>
-                            <p>{photo.caption}</p>
-                        </div>
+                        <p>{photo.caption}</p>
                     </div>))}
                 </ReactSwipe>
                 <div className='controls'>
